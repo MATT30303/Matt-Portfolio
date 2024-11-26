26aae80491435a180e9c971e79d1f085864bbed5
Matt <fabianbarzola2003@gmail.com>
2024-11-26 12:35:07 -0300

    feat: page switch

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import Home from "./pages/Home";
 import About from "./pages/About";
 import { Skull, Navbar } from "./components/global";
+import { useState } from "react";
 function App() {
+  const [Nav, setNav] = useState(0);
+  const navSelect = (num) => {
+    setNav(num);
+  };
+  console.log(Nav);
+  const pages = [<Home />, <About />];
+
   return (
     <>
       <Skull></Skull>
-      <Navbar></Navbar>
-      <Home></Home>
-      <About></About>
+      <Navbar Nav={Nav} navSelect={navSelect}></Navbar>
+      {pages[Nav]}
     </>
   );
 }
--- src/components/global/Navbar.jsx
+++ src/components/global/Navbar.jsx
@@ -1,18 +1,45 @@
 import "../../assets/styles/global.css";
 import styles from "./Navbar.module.css";
-export default function Navbar() {
+export default function Navbar({ nav, navSelect }) {
+  const selector = (num) => {
+    if (nav === num) {
+      return;
+    }
+    navSelect(num);
+  };
+
   return (
     <div className={styles.navbar}>
-      <div className={styles.home}>
+      <div
+        className={styles.home}
+        onClick={() => {
+          selector(0);
+        }}
+      >
         <span className={styles.homeTxt}>Home</span>
       </div>
-      <div className={styles.about}>
+      <div
+        className={styles.about}
+        onClick={() => {
+          selector(1);
+        }}
+      >
         <span className={styles.aboutTxt}>About</span>
       </div>
-      <div className={styles.projects}>
+      <div
+        className={styles.projects}
+        onClick={() => {
+          selector(2);
+        }}
+      >
         <span className={styles.projectsTxt}>Projects</span>
       </div>
-      <div className={styles.contact}>
+      <div
+        className={styles.contact}
+        onClick={() => {
+          selector(3);
+        }}
+      >
         <span className={styles.contactTxt}>Contact</span>
       </div>
     </div>
