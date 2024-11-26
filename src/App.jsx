441662bde2ccfd57a45727e8fd37eae8c2825ba5
Matt <fabianbarzola2003@gmail.com>
2024-11-26 11:33:11 -0300

    refactor: restructure code

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import Home from "./pages/Home";
-
+import About from "./pages/About";
+import { Skull, Navbar } from "./components/global";
 function App() {
   return (
     <>
+      <Skull></Skull>
+      <Navbar></Navbar>
       <Home></Home>
+      <About></About>
     </>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,19 @@
 import "../assets/styles/global.css";
 import Styles from "./Home.module.css";
-import { Skull, Navbar } from "../components/global";
 export default function Home() {
   return (
-    <>
-      <Skull></Skull>
-      <article className={Styles.home}>
-        <Navbar></Navbar>
-        <div className={Styles.flan}></div>
-        <section className={Styles.textContainer}>
-          <span className={Styles.note}>Welcome to my portfolio</span>
-          <div className={Styles.introBox}>
-            <span className={Styles.introText}>
-              I&apos;m Fabián, a passionate web developer dedicated to building
-              intuitive, modern web experiences with clean, impactful designs.
-              Let&apos;s dive in!
-            </span>
-          </div>
-        </section>
-      </article>
-    </>
+    <article className={Styles.home}>
+      <div className={Styles.flan}></div>
+      <section className={Styles.textContainer}>
+        <span className={Styles.note}>Welcome to my portfolio</span>
+        <div className={Styles.introBox}>
+          <span className={Styles.introText}>
+            I&apos;m Fabián, a passionate web developer dedicated to building
+            intuitive, modern web experiences with clean, impactful designs.
+            Let&apos;s dive in!
+          </span>
+        </div>
+      </section>
+    </article>
   );
 }
